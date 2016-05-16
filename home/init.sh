e6e7d2e24d19329e91eb7d274b75e3a588a86746
Cason Clagg <cason@cason.cc>
2016-05-15 22:19:54 -0500

    Put brew install back into install mac script

--- home/init.sh
+++ home/init.sh
@@ -1,17 +1,3 @@
-if test ! $(which brew); then
-  echo "Installing Xcode ..."
-  xcode-select --install
-
-  echo "Installing Homebrew ..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "Updating Homebrew ..."
-  brew update
-  brew upgrade
-fi
-brew doctor
-brew tap homebrew/dupes
-
 echo "Installing homesick"
 sudo gem install homesick
 homesick clone casoninabox/dotfiles-full
